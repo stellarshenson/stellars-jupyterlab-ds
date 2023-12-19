fd4e182c5a23c8c3cfc1484872afd433f82dd113
stellarshenson <konrad.jelen@gmail.com>
2023-12-19 05:48:58 +0100

    updated to support cuda

--- etc/.jupyter/jupyter_lab_config.py
+++ etc/.jupyter/jupyter_lab_config.py
@@ -4,7 +4,7 @@ c = get_config()  #noqa
 c.ServerApp.browser = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
 c.ServerApp.root_dir = "/opt/workspace"
 c.FileContentsManager.always_delete_dir = True
-c.NotebookApp.terminado_settings = { "shell_command": ["/bin/bash"] }
+c.NotebookApp.terminado_settings = { "shell_command": ["/conda_entry.sh", "/bin/bash"] }
 
 #------------------------------------------------------------------------------
 # Application(SingletonConfigurable) configuration
