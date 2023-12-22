8427f4b706a7a55d63b12a55ef49d6c353e62391
stellarshenson <konrad.jelen@gmail.com>
2023-12-22 10:48:06 +0100

    updated git

--- build/conf/start-jupyterlab.sh
+++ build/conf/start-jupyterlab.sh
@@ -7,5 +7,9 @@ find $HOME_TEMPLATE -type d | xargs chmod 700
 find $HOME_TEMPLATE -type f | xargs chmod 600
 cp -rf $HOME_TEMPLATE/. /root
 
+# allow container's git work with imported folders
+# git config --global --add safe.directory '*'
+
+
 # run jupyterlab
 jupyter-lab --ip='*' --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
