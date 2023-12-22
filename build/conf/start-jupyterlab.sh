6c5d9b1aff24aaee130fd46ad1ee2c8eb7c98aff
stellarshenson <konrad.jelen@gmail.com>
2023-12-22 08:03:07 +0100

    fixed home directory settings sync with container

--- build/conf/start-jupyterlab.sh
+++ build/conf/start-jupyterlab.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# copy template home directory to /root
+# and make sure /root permissions are as they should
+HOME_TEMPLATE=/mnt/home
+find $HOME_TEMPLATE -type d | xargs chmod 700
+find $HOME_TEMPLATE -type f | xargs chmod 600
+cp -rf $HOME_TEMPLATE/. /root
+
+# run jupyterlab
 jupyter-lab --ip='*' --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
