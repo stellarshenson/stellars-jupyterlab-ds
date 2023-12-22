79bc2dc2e09712e9370a863cb5d7fbb215d5f13f
stellarshenson <konrad.jelen@gmail.com>
2023-12-22 11:56:49 +0100

    finished preparing the container - release 1.0

--- build/conf/start-jupyterlab.sh
+++ build/conf/start-jupyterlab.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # copy template home directory to /root
-# and make sure /root permissions are as they should
+# dump warnings to /dev/null
 HOME_TEMPLATE=/mnt/home
-find $HOME_TEMPLATE -type d | xargs chmod 700
-find $HOME_TEMPLATE -type f | xargs chmod 600
-cp -rf $HOME_TEMPLATE/. /root
+cp -rf $HOME_TEMPLATE/. /root 2>/dev/null
+
+# and make sure /root permissions are as they should
+find $HOME -type d | xargs chmod 700
+find $HOME -type f | xargs chmod 600
 
 # allow container's git work with imported folders
 # git config --global --add safe.directory '*'
-
 
 # run jupyterlab
 jupyter-lab --ip='*' --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
