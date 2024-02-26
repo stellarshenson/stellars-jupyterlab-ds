bf218a2e355492e5393d587bc78380f881b5a7dc
stellarshenson <konrad.jelen@gmail.com>
2024-02-26 13:17:50 +0100

    reorganised config files

---
+++ build/conf/bin/start-jupyter.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# copy template home directory to /root
+# dump warnings to /dev/null
+HOME_TEMPLATE=/mnt/home
+cp -rf --no-preserve=mode,ownership $HOME_TEMPLATE/. /root 2>/dev/null
+
+# and make sure /root permissions are as they should
+find $HOME -type d | xargs chmod og-rwx
+find $HOME -type f | xargs chmod og-rwx
+
+# output build info and banners
+BUILD_DATE=`cat /build-date.txt`
+BUILD_NAME=`cat /build-name.txt`
+
+echo ""
+echo "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█" 
+echo "█ Build '$BUILD_NAME' created on $BUILD_DATE    █" 
+echo "█ ------------------------------------------------------  █" 
+echo "█ Running JupyterLab server on  http://localhost:8888     █"
+echo "█ Running Tensorboard server on http://localhost:6006     █"
+echo "█ Tensorboard monitoring logs located in /tmp/tf_logs     █"
+echo "█ Using work dir (projects) /opt/workspace                █"
+echo "█ Jupyterlab settings saved to /root/.jupyter             █"
+echo "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo ""
+
+# run tensorboard in the background
+mkdir /tmp/tensorboard 2>/dev/null
+tensorboard --bind_all --logdir /tmp/tf_logs --port 6006 &
+
+# run jupyterlab
+jupyter-lab --ip='*' --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
