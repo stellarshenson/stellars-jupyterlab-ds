64c8e1fdafe8c83fb1226cdc42958147e322dcd4
stellarshenson <konrad.jelen@gmail.com>
2024-03-03 22:29:41 +0100

    updated launchers to not build by default (but pull instead)

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh 
-docker-compose -f ../docker-compose.yml up --no-recreate
+docker-compose -f ../docker-compose.yml up  --no-recreate --no-build 
--- bin/start_nvidia.sh
+++ bin/start_nvidia.sh
@@ -1,2 +1,2 @@
 #!/bin/sh 
-docker-compose -f ../docker-compose-nvidia.yml up --no-recreate
+docker-compose -f ../docker-compose-nvidia.yml up --no-recreate --no-build 
