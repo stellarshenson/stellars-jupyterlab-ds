48efdf3aa3d3fdd4f029d678d2fc9b96159988f7
stellarshenson <konrad.jelen@gmail.com>
2024-05-27 20:24:42 +0200

    changed the way home folder is handled -> volumes

--- build/conf/bin/start-jupyterlab.sh
+++ build/conf/bin/start-jupyterlab.sh
@@ -6,12 +6,12 @@ if [[ $GPU_SUPPORT_ENABLED == 1 ]]; then
 fi
 
 # copy template home directory to /root, dump warnings to /dev/null
-HOME_TEMPLATE=/mnt/home
-cp -rf --no-preserve=mode,ownership $HOME_TEMPLATE/. $HOME 2>/dev/null
+#HOME_TEMPLATE=/mnt/home
+#cp -rf --no-preserve=mode,ownership $HOME_TEMPLATE/. $HOME 2>/dev/null
 
 # and make sure home permissions are as they should
-find $HOME -type d -printf "'%p'\n" | xargs chmod 700
-find $HOME -type f -printf "'%p'\n" | xargs chmod 600
+#find $HOME -type d -printf "'%p'\n" | xargs chmod 700
+#find $HOME -type f -printf "'%p'\n" | xargs chmod 600
 
 # output build info and banners
 BUILD_DATE=`cat /build-date.txt`
@@ -24,9 +24,9 @@ mkdir /tmp/tensorboard 2>/dev/null
 tensorboard --bind_all --logdir /tmp/tf_logs --port 6006 &
 
 # generate ssl keys if don't exist yet (happens first time the script is run)
-CERT_DIR="/mnt/certs"
-if [ ! -e "$CERT_DIR/jupyterlab.crt" ]; then
-	/generate_jupyterlab_ssl.sh "$CERT_DIR"
+CERTS_DIR="/mnt/certs"
+if [ ! -e "$CERTS_DIR/jupyterlab.crt" ]; then
+	/generate_jupyterlab_ssl.sh "$CERTS_DIR"
 fi
 
 # run jupyterlab
--- home/.bash_aliases
+++
@@ -1,1 +0,0 @@
-alias mc='mc -b'
