5c86b58c312895f88dcf1b87fbc793ea3f1d4363
stellarshenson <konrad.jelen@gmail.com>
2024-06-01 06:01:23 +0200

    added logging

--- build/conf/bin/start-jupyterlab.sh
+++ build/conf/bin/start-jupyterlab.sh
@@ -5,18 +5,9 @@ if [[ $GPU_SUPPORT_ENABLED == 1 ]]; then
     /usr/bin/nvidia-smi
 fi
 
-# copy template home directory to /root, dump warnings to /dev/null
-#HOME_TEMPLATE=/mnt/home
-#cp -rf --no-preserve=mode,ownership $HOME_TEMPLATE/. $HOME 2>/dev/null
-
-# and make sure home permissions are as they should
-#find $HOME -type d -printf "'%p'\n" | xargs chmod 700
-#find $HOME -type f -printf "'%p'\n" | xargs chmod 600
-
 # output build info and banners
 BUILD_DATE=`cat /build-date.txt`
 BUILD_NAME=`cat /build-name.txt`
-
 cat /build-info.txt | sed "s/@BUILD_NAME@/$BUILD_NAME/g" | sed "s/@BUILD_DATE@/$BUILD_DATE/g" 
 
 # run tensorboard in the background
@@ -29,7 +20,8 @@ if [ ! -e "$CERTS_DIR/jupyterlab.crt" ]; then
 	/generate_jupyterlab_ssl.sh "$CERTS_DIR"
 fi
 
-# run jupyterlab
-jupyter-lab --ip='*' --no-browser --IdentityProvider.token='' --IdentityProvider.password=''
+# run jupyterlab, env params are configured in Dockerfile and docker-compose yml 
+jupyter-lab --ip=$JUPYTERLAB_SERVER_IP --IdentityProvider.token=$JUPYTERLAB_SERVER_TOKEN \
+    --IdentityProvider.password=$JUPYTERLAB_SERVER_PASSWORD --no-browser 
 
 # EOF
