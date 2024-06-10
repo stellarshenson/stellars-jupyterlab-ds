e3af45c9964e6e6ed6ff93f684d6fa2aeebf94f0
stellarshenson <konrad.jelen@gmail.com>
2024-06-10 14:12:05 +0200

    fixed docker-compose project name

--- bin/start.sh
+++ bin/start.sh
@@ -4,9 +4,9 @@ CURRENT_DIR=`dirname $CURRENT_FILE`
 cd $CURRENT_DIR
 
 if [ "$1" = "-d" ]; then
-    docker-compose -f ../docker-compose.yml up  --no-recreate --no-build &
+    docker-compose -p stellars-jupyterlab-ds -f ../docker-compose.yml up  --no-recreate --no-build &
 else
-    docker-compose -f ../docker-compose.yml up  --no-recreate --no-build 
+    docker-compose -p stellars-jupyterlab-ds -f ../docker-compose.yml up  --no-recreate --no-build 
 fi
 
 # EOF
