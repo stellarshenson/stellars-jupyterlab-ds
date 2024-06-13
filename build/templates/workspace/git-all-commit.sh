e504252863667f435fd31d31cf684cc53db2e3f8
stellarshenson <konrad.jelen@gmail.com>
2024-06-14 01:10:42 +0200

    added helper scripts to pull and commit across all repos in ws

---
+++ build/templates/workspace/git-all-commit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+OIFS="$IFS" # old separator
+IFS=$'\n' # new separator - newline
+CURRENT_DIR=$(pwd)
+
+# list repos, but exclude @archive and tutorials
+REPOS=$(find . -name '.git' -type d -printf "%p\n" | grep -v 'tutorials' | grep -v '@archive')
+
+# execute git pull
+for r in $REPOS;
+do
+    cd $CURRENT_DIR
+    echo "executing commit for $r"
+    cd $(realpath $r)
+    cd ..
+    git commit -a
+done
+
+# go back
+cd $CURRENT_DIR
+
+IFS="$OIFS" # restore separator
