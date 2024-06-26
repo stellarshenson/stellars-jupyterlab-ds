430b8aae464506f46e39527f20748e3f870950fa
stellarshenson <konrad.jelen@gmail.com>
2024-06-26 05:39:42 +0200

    fixed launch scripts

--- bin/start_nvidia.sh
+++ bin/start_nvidia.sh
@@ -3,6 +3,6 @@ CURRENT_FILE=`readlink -f $0`
 CURRENT_DIR=`dirname $CURRENT_FILE`
 cd $CURRENT_DIR
 
-docker-compose -p stellars-jupyterlab-ds -f ../docker-compose-nvidia.yml up --no-recreate --no-build $1
+docker-compose -f ../docker-compose-nvidia.yml up --no-recreate --no-build $1
 
 # EOF
