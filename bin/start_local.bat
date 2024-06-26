565e8d9dcd043e90b1d0e331cd260583b318dee5
stellarshenson <konrad.jelen@gmail.com>
2024-06-26 02:19:13 +0200

    updated windows script with nvidia detector

--- bin/start_local.bat
+++ bin/start_local.bat
@@ -1,16 +1,17 @@
 @echo off
-REM Check if GPU is available
-powershell -Command "if ((Get-WmiObject Win32_VideoController | Where-Object { $_.Name -ne $null }).Count -gt 0) { exit 0 } else { exit 1 }"
+
+REM Check for Nvidia GPU using wmic
+wmic path win32_VideoController get name | findstr /i "NVIDIA" >nul
 
 REM Capture the exit code
 set gpu_available=%errorlevel%
 
 REM Execute commands based on GPU availability
 if %gpu_available% equ 0 (
-    echo GPU is available. Running local docker-compose.yml
+    echo Nvidia GPU is available. Running local docker-compose.yml
     docker-compose.exe -f ..\local\docker-compose-nvidia.yml up --no-recreate --no-build 
 ) else (
-    echo GPU is not available. Running local docker-compose-nvidia.yml
+    echo Nvidia GPU is not available. Running local docker-compose-nvidia.yml
     docker-compose.exe -f ..\local\docker-compose.yml up --no-recreate --no-build 
 )
 
