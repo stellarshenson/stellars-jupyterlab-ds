47c6287befc110584de37cfe85d5e94c78d22961
stellarshenson <konrad.jelen@gmail.com>
2024-06-26 02:14:57 +0200

    updated windows script for local

--- bin/start_local.bat
+++ bin/start_local.bat
@@ -1,2 +1,19 @@
-docker-compose.exe -f ..\local\docker-compose.yml up --no-recreate --no-build 
+@echo off
+REM Check if GPU is available
+powershell -Command "if ((Get-WmiObject Win32_VideoController | Where-Object { $_.Name -ne $null }).Count -gt 0) { exit 0 } else { exit 1 }"
+
+REM Capture the exit code
+set gpu_available=%errorlevel%
+
+REM Execute commands based on GPU availability
+if %gpu_available% equ 0 (
+    echo GPU is available. Running local docker-compose.yml
+    docker-compose.exe -f ..\local\docker-compose-nvidia.yml up --no-recreate --no-build 
+) else (
+    echo GPU is not available. Running local docker-compose-nvidia.yml
+    docker-compose.exe -f ..\local\docker-compose.yml up --no-recreate --no-build 
+)
+
+REM EOF
+
 
