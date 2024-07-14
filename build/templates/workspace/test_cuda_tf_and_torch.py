8ab144036c77a7bf24b7324501821fd5ce60ed27
stellarshenson <konrad.jelen@gmail.com>
2024-07-15 01:13:23 +0200

    updated dockerfile with ENV compliance and updated test script

--- build/templates/workspace/test_cuda_tf_and_torch.py
+++ build/templates/workspace/test_cuda_tf_and_torch.py
@@ -1,8 +1,9 @@
 #!/opt/conda/bin/python
 
-import tensorflow as tf
-import torch
 from colorama import Fore, Back, Style
+import os
+import sys
+
 
 def coloured_text(text, colour="white", bg_colour="normal", style="normal") -> str:
     """
@@ -60,7 +61,10 @@ def coloured_print(text, colour="white", bg_colour="normal", style="normal"):
 
 
 def check_tensorflow():
-    is_gpu = len(tf.config.list_physical_devices("GPU")) > 0
+    # only initialise tensorflow when called  (prevents segfault because of conflict with torch)
+    import tensorflow as tf
+
+    is_gpu = len(tf.config.list_physical_devices("GPU")) > 0    
     colour = 'green' if is_gpu == True else 'red'
     
     # check for tensorflow devices
@@ -77,15 +81,18 @@ def check_tensorflow():
         coloured_print(f'Tensorflow CUDNN version: {tf.sysconfig.get_build_info()["cudnn_version"]}', colour=colour)
     
     # if gpu available, print its name
-    gpu_devices = tf.config.list_physical_devices('GPU')
-    coloured_print(f'Tensorflow found CUDA devices: {len(gpu_devices)}', colour=colour)
-    if len(gpu_devices) > 0:
-        for i in range(0, len(gpu_devices)):
-            gpu_details = tf.config.experimental.get_device_details(gpu_devices[i])
+    gpu_devices_list = tf.config.list_physical_devices('GPU')
+    coloured_print(f'Tensorflow found CUDA devices: {len(gpu_devices_list)}', colour=colour)
+    if len(gpu_devices_list) > 0:
+        for i in range(0, len(gpu_devices_list)):
+            gpu_details = tf.config.experimental.get_device_details(gpu_devices_list[i])
             coloured_print(f'GPU {i}: {gpu_details["device_name"]}', colour=colour)
-
+        
 
 def check_pytorch():
+    # only initialise torch when called (prevents segfault)
+    import torch
+    
     is_gpu = torch.cuda.device_count() > 0
     colour = 'green' if is_gpu == True else 'red'
     
@@ -97,14 +104,27 @@ def check_pytorch():
     coloured_print(f'pyTorch built with CUDA support: {torch.cuda.is_available()}', colour=colour)
     if torch.cuda.is_available(): coloured_print(f"pyTorch CUDA version: {torch.version.cuda}", colour=colour)
     coloured_print(f'pyTorch found CUDA devices: {torch.cuda.device_count()}', colour=colour)
+
     if torch.cuda.device_count() > 0:
         for i in range(0, torch.cuda.device_count()):
-            coloured_print(f'pyTorch CUDA device {i}: {torch.cuda.get_device_name(i)}', colour=colour)
-    
-    coloured_print('', colour=colour)
+            coloured_print(f'GPU {i}: {torch.cuda.get_device_name(i)}', colour=colour)
+
 
 if __name__ == '__main__':
-    check_tensorflow()
-    check_pytorch()
+    # Open /dev/null and get its file descriptor
+    with open(os.devnull, 'w') as devnull:
+        devnull_fd = devnull.fileno()
+        # Duplicate the original stderr file descriptor
+        original_stderr_fd = os.dup(sys.stderr.fileno())
 
+        # Redirect stderr to /dev/null
+        os.dup2(devnull_fd, sys.stderr.fileno())
+
+        check_pytorch() # need to call before tensorflow    
+        check_tensorflow()
+        print('')
+
+        # Restore stderr
+        os.dup2(original_stderr_fd, sys.stderr.fileno())
+    
 # EOF
