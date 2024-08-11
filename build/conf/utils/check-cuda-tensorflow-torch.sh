7c7a6f3f8a12fed4b864c0144cb3e0aabd573cf9
stellarshenson <konrad.jelen@gmail.com>
2024-08-11 13:49:57 +0200

    fixed bugs with cuda test

--- build/conf/utils/check-cuda-tensorflow-torch.sh
+++ build/conf/utils/check-cuda-tensorflow-torch.sh
@@ -1,3 +1,3 @@
-/conda-run.sh "CONDA_DEFAULT_ENV=tensorflow python /check-cuda-tensorflow-torch.py"
-/conda-run.sh "CONDA_DEFAULT_ENV=torch python /check-cuda-tensorflow-torch.py"
+/conda-run.sh "CONDA_DEFAULT_ENV=tensorflow python /opt/utils/check-cuda-tensorflow-torch.py"
+/conda-run.sh "CONDA_DEFAULT_ENV=torch python /opt/utils/check-cuda-tensorflow-torch.py"
 echo ""
