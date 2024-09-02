059867926b88933aafa673fbc522942c05406d53
stellarshenson <konrad.jelen@gmail.com>
2024-09-02 16:33:27 +0200

    added updated docker start script

--- extra/windows/docker.bat
+++ extra/windows/docker.bat
@@ -2,7 +2,7 @@
 echo Waking up WSL
 wsl.exe --exec "true"
 
-timeout /t 10 /nobreak
+timeout /t 5 /nobreak
 
 echo Running Docker Desktop
 start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe" -Autostart
