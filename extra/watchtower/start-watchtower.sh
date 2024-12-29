fabf5d0ec4714173067bd6392a4d851984f4e533
stellarshenson <konrad.jelen@gmail.com>
2024-12-29 21:07:39 +0100

    added watchtower service

---
+++ extra/watchtower/start-watchtower.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+echo "starting watchtower to attempt refresh containers every 1h"
+docker run -d \
+  --name watchtower \
+  --restart unless-stopped \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  containrrr/watchtower \
+  --interval-seconds 3600
+
