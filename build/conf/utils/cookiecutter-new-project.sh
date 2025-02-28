67065750998bba130c86abdfb3daa9706fc06a11
stellarshenson <konrad.jelen@gmail.com>
2025-02-28 13:27:24 +0100

    updated to allow create new project while in any env

--- build/conf/utils/cookiecutter-new-project.sh
+++ build/conf/utils/cookiecutter-new-project.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # create new project using cookiecutter
- cookiecutter https://github.com/stellarshenson/cookiecutter-data-science.git
+conda run -n base cookiecutter https://github.com/stellarshenson/cookiecutter-data-science.git
 #cookiecutter ./cookiecutter-data-science
