3e1013eea0e1b898de515e207e69120ec03156e4
stellarshenson <konrad.jelen@gmail.com>
2025-05-16 19:14:34 +0200

    updated with variables from env file

--- extra/watchtower/start-watchtower.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-echo "starting watchtower to attempt refresh containers every 1h"
-docker run -d \
-  --name watchtower \
-  --restart unless-stopped \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  containrrr/watchtower \
-  --interval 3600
-
