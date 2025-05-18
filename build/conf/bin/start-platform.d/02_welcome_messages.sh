73f9bcd4b1ae4146eac5e0e44568ad5f01c22022
stellarshenson <konrad.jelen@gmail.com>
2025-05-18 02:31:23 +0200

    fixed welcome message

--- build/conf/bin/start-platform.d/02_welcome_messages.sh
+++ build/conf/bin/start-platform.d/02_welcome_messages.sh
@@ -4,15 +4,16 @@
 # ----------------------------------------------------------------------------------------
 
 # execute render-info.py script to display info box
-conda run -n base python /render-info.py
-
+BUILD_NAME=$(cat /build-name.txt)
+BUILD_DATE=$(cat /build-date.txt)
+conda run -n base python /render-info.py "$LAB_NAME" "$BUILD_NAME" "$BUILD_DATE" "$JUPYTERLAB_SERVER_TOKEN"
 
 # update welcome.html with LAB_USER
 # it is a bit tricky because we can only do inplace changes
 cat /welcome.html | \
-    sed "s/@LAB_USER@/${LAB_USER:-default}/g" \
-    sed "s/@LAB_NAME@/${LAB_NAME:-stellars-jupyterlab-ds}/g" \
-    > /tmp/welcome.html.new
+/usr/bin/sed "s/@LAB_USER@/${LAB_USER:-default}/g" \
+/usr/bin/sed "s/@LAB_NAME@/${LAB_NAME:-stellars-jupyterlab-ds}/g" \
+> /tmp/welcome.html.new
 
 truncate /welcome.html --size 0
 cat /tmp/welcome.html.new >> /welcome.html
