0639cace5bac815b5c24a82abf386ac6f04207cd
stellarshenson <konrad.jelen@gmail.com>
2025-05-20 09:51:53 +0200

    updated names of the scripts

--- build/conf/utils/check-cuda-tensorflow-torch.sh
+++
@@ -1,3 +0,0 @@
-/conda-run.sh "CONDA_DEFAULT_ENV=tensorflow python /opt/utils/check-cuda-tensorflow-torch.py"
-/conda-run.sh "CONDA_DEFAULT_ENV=torch python /opt/utils/check-cuda-tensorflow-torch.py"
-echo ""
---
+++ build/conf/utils/test-cuda-tensorflow-torch.sh
@@ -0,0 +1,3 @@
+/conda-run.sh "CONDA_DEFAULT_ENV=tensorflow python /opt/utils/test-cuda-tensorflow-torch.py"
+/conda-run.sh "CONDA_DEFAULT_ENV=torch python /opt/utils/test-cuda-tensorflow-torch.py"
+echo ""
