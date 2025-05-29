98e4aa1e620ea6b646ee832b80f9b69d3e5c96a1
stellarshenson <konrad.jelen@gmail.com>
2025-05-29 13:21:05 +0200

    updated certificates generator

--- multi-user/start_lab_user.sh
+++ multi-user/start_lab_user.sh
@@ -77,7 +77,6 @@ services:
     # generate SSL certificates and enter idle mode
     entrypoint: "/bin/bash -c '/start-platform.d/00_generate_ssl_cert.sh && tail -f /dev/null'"  
     container_name: \${COMPOSE_PROJECT_NAME:-lab-admin}-certificates-generator
-    restart: no
 EOF
 
 
