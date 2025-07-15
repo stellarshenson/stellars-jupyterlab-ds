76b84335dc86c7670c02ad63c4eac631da42180b
stellarshenson <konrad.jelen@gmail.com>
2025-07-15 16:55:34 +0200

    adopted to allow spawning in the context of jupyterhub

--- build/conf/bin/start-platform.sh
+++ build/conf/bin/start-platform.sh
@@ -10,12 +10,19 @@ for file in $START_PLATFORM_DIR/*; do
 done
 
 # run jupyterlab, env params are configured in Dockerfile and docker-compose yml 
-jupyter-lab \
-    --autoreload \
-    --ip=$JUPYTERLAB_SERVER_IP \
-    --IdentityProvider.token=$JUPYTERLAB_SERVER_TOKEN \
-    --ServerApp.base_url=$JUPYTERLAB_BASE_URL \
-    --no-browser 
+if [[ -z ${JUPYTERLAB_STARTUP_MODE} ||  ${JUPYTERLAB_STARTUP_MODE} == 'jupyterlab' ]]; then
+    echo "starting jupyterlab server"
+    jupyter-lab \
+	--autoreload \
+	--ip=$JUPYTERLAB_SERVER_IP \
+	--IdentityProvider.token=$JUPYTERLAB_SERVER_TOKEN \
+	--ServerApp.base_url=$JUPYTERLAB_BASE_URL \
+	--no-browser 
+# run in jupyterhub mode
+elif [[ ${JUPYTERLAB_STARTUP_MODE} == 'jupyterhub' ]]; then 
+    echo "starting jupyterhub-singleuser server"
+    jupyterhub-singleuser
+fi
 
 # EOF
 
