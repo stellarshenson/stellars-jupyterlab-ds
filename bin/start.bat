1377f7f5abaf2dd1c63cd55a0b36a734b386fc9e
stellarshenson <konrad.jelen@gmail.com>
2025-07-16 07:00:26 +0200

    changed structure

--- bin/start.bat
+++ bin/start.bat
@@ -12,10 +12,10 @@ set gpu_available=%errorlevel%
 REM Execute commands based on GPU availability
 if %gpu_available% equ 0 (
     echo NVIDIA GPU is available
-    docker.exe compose --env-file ..\project.env -f ..\compose.yml -f ..\compose-gpu.yml up --no-recreate --no-build -d
+    docker.exe compose --env-file ..\.env -f ..\compose.yml -f ..\compose-gpu.yml up --no-recreate --no-build -d
 ) else (
     echo NVIDIA GPU is not available
-    docker.exe compose --env-file ..\project.env -f ..\compose.yml up --no-recreate --no-build -d
+    docker.exe compose --env-file ..\.env -f ..\compose.yml up --no-recreate --no-build -d
 )
 
 REM EOF
--- bin/start.sh
+++ bin/start.sh
@@ -8,20 +8,20 @@ if command -v nvidia-smi &> /dev/null; then
     if nvidia-smi > /dev/null 2>&1; then
         echo "Nvidia GPU found."
         # Run the command for when GPU is available
-	docker compose --env-file ../project.env \
+	docker compose --env-file ../.env \
 	    -f ../compose.yml -f ../compose-gpu.yml \
 	    up --no-recreate --no-build -d
     else
         echo "Nvidia GPU not found."
         # Run the command for when GPU is not available
-	docker compose --env-file ../project.env \
+	docker compose --env-file ../.env \
 	    -f ../compose.yml \
 	    up  --no-recreate --no-build -d
     fi
 else
     echo "nvidia-smi command not found. Nvidia GPU not available."
     # Run the command for when GPU is not available
-    docker compose --env-file ../project.env \
+    docker compose --env-file ../.env \
 	-f ../compose.yml \
 	up  --no-recreate --no-build -d
 fi
