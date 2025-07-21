682b22fc9be0c8a03396e48e84daa113adc7689a
stellarshenson <konrad.jelen@gmail.com>
2025-07-21 21:37:19 +0000

    fixed welcome message

--- services/jupyterlab/conf/bin/welcome-message.sh
+++ services/jupyterlab/conf/bin/welcome-message.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-MESSAGE=$(cat /welcome-message.txt | sed s/@LAB_NAME@/${LAB_NAME}/g)
 TIMESTAMP_FILE="$HOME/.last_message_timestamp"
 
 # Get the current date
 CURRENT_DATE=$(date +%Y-%m-%d)
+
+# render message differently depending if hub or standalone jl
+if [[ -n ${JUPYTERHUB_USER} ]]; then
+    BASE_URL=${JUPYTERHUB_SERVICE_PREFIX}
+    BASE_URL=$(echo ${BASE_URL} | sed 's/\/$//g' | sed 's/^\///g' | sed 's/\//\\\//g' )
+    MESSAGE=$(cat /welcome-message.txt | sed "s/@LAB_NAME@/${BASE_URL}/g")
+else
+    MESSAGE=$(cat /welcome-message.txt | sed "s/@LAB_NAME@/${LAB_NAME}\/jupyterlab/g")
+fi
 
 # Check if the timestamp file exists
 if [ -f "$TIMESTAMP_FILE" ]; then
