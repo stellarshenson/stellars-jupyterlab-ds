97954a6795dbf71aae3efdc2f0f2252bf025f9a9
stellarshenson <konrad.jelen@gmail.com>
2025-07-23 23:50:43 +0200

    updated names of the enable flags i.e. ENABLE_GPU_SUPPORT
    also implemented local scripts launcher for user custom scripts

---
+++ services/jupyterlab/conf/bin/start-platform.d/99_start_lab_scripts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------------------
+# Run Local Scripts from user home
+#
+# This script runs all available scripts in user home directory
+# ----------------------------------------------------------------------------------------
+
+# check if enabled
+if [[ ${ENABLE_LOCAL_SCRIPTS} != 1 ]]; then
+    exit 0
+fi
+
+# run series of local start scripts
+# (services will need to run in background)
+LOCAL_SCRIPTS_DIR="${LOCAL_SCRIPTS_DIR:-${HOME}/.local/start-platform.d}"
+if [[ -d "${LAB_SCRIPTS_DIR}" ]]; then
+    echo "executing local scripts from ${LOCAL_SCRIPTS_DIR}"
+    for file in ${LAB_SCRIPTS_DIR}/*; do
+	if [ -f "$file" ] && [ -x "$file" ]; then
+	    "$file" 
+	fi
+    done
+fi
+
+# EOF
