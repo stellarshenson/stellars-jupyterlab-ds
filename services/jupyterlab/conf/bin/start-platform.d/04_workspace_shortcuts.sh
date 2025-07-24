d55e6ad9c4af9a6ab48620c3db38655c9c025b7f
stellarshenson <konrad.jelen@gmail.com>
2025-07-24 16:24:35 +0200

    fixed issues with the scripts

--- services/jupyterlab/conf/bin/start-platform.d/04_workspace_shortcuts.sh
+++ services/jupyterlab/conf/bin/start-platform.d/04_workspace_shortcuts.sh
@@ -5,12 +5,12 @@
 CONDA_USER_WORKSPACE=${CONDA_USER_WORKSPACE:-/home/lab/workspace}
 
 echo "Creating useful shortcuts in the workspace"
-if [[ ! -f "${CONDA_USER_WORKSPACE}/@shared" ]]; then
-    ln -s /mnt/shared ${CONDA_USER_WORKSPACE}/@shared 2>&1 >/dev/null
+if [[ ! -d "${CONDA_USER_WORKSPACE}/@shared" ]]; then
+    ln -s /mnt/shared ${CONDA_USER_WORKSPACE}/@shared 
 fi
 
-if [[ ! -f "${CONDA_USER_WORKSPACE}/@cache" ]]; then
-    ln -s /home/lab/.cache ${CONDA_USER_WORKSPACE}/@cache 2>&1 >/dev/null
+if [[ ! -d "${CONDA_USER_WORKSPACE}/@cache" ]]; then
+    ln -s /home/lab/.cache ${CONDA_USER_WORKSPACE}/@cache 
 fi
 
 # EOF
