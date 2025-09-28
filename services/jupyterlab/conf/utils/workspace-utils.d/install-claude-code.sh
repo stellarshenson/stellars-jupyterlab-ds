847f6895f65d46908ba3e4f1f6482df8d576995b
stellarshenson <konrad.jelen@gmail.com>
2025-09-28 19:51:31 +0200

    updated UX of claude code installer

--- services/jupyterlab/conf/utils/workspace-utils.d/install-claude-code.sh
+++ services/jupyterlab/conf/utils/workspace-utils.d/install-claude-code.sh
@@ -2,10 +2,14 @@
 ## Installs Claude Code Assistant
 
 conda install -y --update-all -n base nodejs
-conda run -n base npm install npm install -g @anthropic-ai/claude-code
-conda run -n base npm update
+conda run -n base npm install npm install -g @anthropic-ai/claude-code && npm -g update
 
 clear
-echo "Claude Code Installation Successful"
-echo "Type \033[32mclaude\033[0m to start"
+echo -e "\033[32mClaude Code Installation Successful\033[0m"
 echo ""
+echo -e "Typical Usage:"
+echo -e "1. Enter your project repository inside the workspace"
+echo -e "2. Type '\033[36mclaude\033[0m' to start working with Claude Code"
+echo ""
+
+# EOF
