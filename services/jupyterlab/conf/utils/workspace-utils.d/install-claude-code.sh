ac8764f37626bf20e559db825f28d2fc00c0d880
stellarshenson <konrad.jelen@gmail.com>
2025-09-28 18:14:07 +0200

    updated

--- services/jupyterlab/conf/utils/workspace-utils.d/install-claude-code.sh
+++ services/jupyterlab/conf/utils/workspace-utils.d/install-claude-code.sh
@@ -6,6 +6,6 @@ conda run -n base npm install npm install -g @anthropic-ai/claude-code
 conda run -n base npm update
 
 clear
-echo "Claude Code Installation Successful
+echo "Claude Code Installation Successful"
 echo "Type \033[32mclaude\033[0m to start"
 echo ""
