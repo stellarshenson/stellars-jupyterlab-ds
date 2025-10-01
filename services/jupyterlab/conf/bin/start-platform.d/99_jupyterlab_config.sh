d6e586f2543ff648e37f127a75cd8e8b73d8a414
stellarshenson <konrad.jelen@gmail.com>
2025-10-01 12:35:27 +0200

    updated jupyterlab config location

--- services/jupyterlab/conf/bin/start-platform.d/99_jupyterlab_config.sh
+++ services/jupyterlab/conf/bin/start-platform.d/99_jupyterlab_config.sh
@@ -7,7 +7,7 @@ CONDA_USER_WORKSPACE=${CONDA_USER_WORKSPACE:-/home/lab/workspace}
 echo "Forcing predefined jupyterlab config file"
 
 JUPYTERLAB_CONFIG_FILE_USER="${HOME}/.jupyter/jupyter_lab_config.py"
-JUPYTERLAB_CONFIG_FILE_TEMPLATE="/opt/etc/jupyter/jupyter_lab_config.py"
+JUPYTERLAB_CONFIG_FILE_TEMPLATE="/opt/conda/etc/jupyter/jupyter_lab_config.py"
 
 if [[ ! -f "${JUPYTERLAB_CONFIG_FILE_USER}" ]]; then
     rm ${JUPYTERLAB_CONFIG_FILE_USER}
