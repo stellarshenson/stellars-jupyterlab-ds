90eb1a7619ead0e82ed7d6e322c2c00641b51f7a
stellarshenson <konrad.jelen@gmail.com>
2025-10-27 12:20:48 +0100

    feat: optimize environment installation and update documentation
    - Fixed Makefile to retrieve latest git tag by creation date
    - Updated README.md to reflect on-demand environment installation model
    - Modified TensorBoard to launch from base environment instead of tensorflow
    - Changed tensorflow/torch installation to apply environment_base_template.yml instead of cloning base environment
    - Added version comment to environment_base_jupyterlab.yml

--- services/jupyterlab/conf/utils/workspace-utils.d/install-conda-env.d/tensorflow.sh
+++ services/jupyterlab/conf/utils/workspace-utils.d/install-conda-env.d/tensorflow.sh
@@ -9,12 +9,13 @@ CONDA_DEFAULT_ENV=${CONDA_DEFAULT_ENV:-base}
 
 echo "Creating conda environment: ${ENV_NAME}"
 
-# Clone base environment
-echo "Cloning ${ENV_NAME} environment from ${CONDA_DEFAULT_ENV}"
-${CONDA_CMD} create --name ${ENV_NAME} --clone ${CONDA_DEFAULT_ENV}
+# Create fresh environment and apply base packages first
+echo "Creating ${ENV_NAME} environment with base packages"
+${CONDA_CMD} create --name ${ENV_NAME} -y
+${CONDA_CMD} env update -v --name ${ENV_NAME} --file=/environment_base_template.yml
 ${CONDA_CMD} clean -a -y
 
-# Install tensorflow using environment file
+# Install tensorflow packages
 echo "Installing tensorflow packages"
 ${CONDA_CMD} env update -v --name ${ENV_NAME} --file=/environment_tensorflow.yml
 ${CONDA_CMD} clean -a -y
