75330b223ddbd3d002b342513db23b1abca1e258
stellarshenson <konrad.jelen@gmail.com>
2025-11-03 19:09:23 +0100

    refactor: remove [INFO] label from tab closure message

--- services/jupyterlab/conf/utils/launch-lab-utils.sh
+++ services/jupyterlab/conf/utils/launch-lab-utils.sh
@@ -27,7 +27,7 @@ lab-utils
 # Pause before exit with info about manual tab closure
 echo ""
 echo "Press Enter to close..."
-echo -e "\033[1;33m[INFO]\033[0m You will need to close this tab manually"
+echo -e "\033[1;33mYou will need to close this tab manually\033[0m"
 read
 
 # Exit terminal session to prevent shell prompt
