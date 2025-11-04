21303dd0e2af28eb3825f72cefd3963e50564490
stellarshenson <konrad.jelen@gmail.com>
2025-11-04 16:33:53 +0100

    fix: create README.md symlink in user's conda-env.d at startup

--- services/jupyterlab/conf/bin/start-platform.d/05_user_directories.sh
+++ services/jupyterlab/conf/bin/start-platform.d/05_user_directories.sh
@@ -11,4 +11,10 @@ if [[ ! -d "/home/lab/.local/conda-env.d" ]]; then
     echo "Created /home/lab/.local/conda-env.d"
 fi
 
+# Create symlink to README.md if it doesn't exist
+if [[ ! -e "/home/lab/.local/conda-env.d/README.md" ]]; then
+    ln -s /opt/utils/conda-env.d/README.md /home/lab/.local/conda-env.d/README.md
+    echo "Created symlink to conda-env.d README.md"
+fi
+
 # EOF
