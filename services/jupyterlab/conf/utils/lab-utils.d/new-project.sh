5b6c63f576f347ec4d7be5eceb076db202cbc2cd
stellarshenson <konrad.jelen@gmail.com>
2025-12-05 18:01:18 +0000

    feat: added new copier based new project scaffolding

--- services/jupyterlab/conf/utils/lab-utils.d/new-project.sh
+++ services/jupyterlab/conf/utils/lab-utils.d/new-project.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-## Scaffolds new data science project based on cookiecutter
+## Scaffolds new data science project using Copier
 
-# create new project using ccds (cookiecutter-data-science)
-conda run --no-capture-output -n base ccds ${COOKIECUTTER_DATASCIENCE_TEMPLATE_URL} --checkout master
+# Ask for project name (magenta bold)
+echo -en "\033[1;35mEnter project name:\033[0m "
+read PROJECT_NAME
+
+if [ -z "$PROJECT_NAME" ]; then
+    echo "Error: Project name cannot be empty"
+    exit 1
+fi
+
+echo ""
+
+# Create project using copier (project_name derived from folder name)
+copier copy --trust \
+    https://github.com/stellarshenson/cookiecutter-data-science.git \
+    "$PROJECT_NAME"
