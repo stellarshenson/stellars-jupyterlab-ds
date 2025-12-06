0b8fbd96bea902fe059940508e22ba1eca141c9d
stellarshenson <konrad.jelen@gmail.com>
2025-12-06 14:34:37 +0000

    feat: display template version in new-project.sh

--- services/jupyterlab/conf/utils/lab-utils.d/new-project.sh
+++ services/jupyterlab/conf/utils/lab-utils.d/new-project.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 ## Scaffolds new data science project using Copier
 
+REPO_URL="https://github.com/stellarshenson/cookiecutter-data-science.git"
+
+# Get latest tag from GitHub
+LATEST_TAG=$(git ls-remote --tags --sort=-v:refname "$REPO_URL" 2>/dev/null | head -n1 | sed 's/.*refs\/tags\///')
+
+# Display template version (cyan)
+if [ -n "$LATEST_TAG" ]; then
+    echo -e "\033[36mUsing template version: $LATEST_TAG\033[0m"
+fi
+
 # Ask for project name (magenta bold)
 echo -en "\033[1;35mEnter project name:\033[0m "
 read PROJECT_NAME
@@ -13,6 +23,4 @@ fi
 echo ""
 
 # Create project using copier (project_name derived from folder name)
-copier copy --trust \
-    https://github.com/stellarshenson/cookiecutter-data-science.git \
-    "$PROJECT_NAME"
+copier copy --trust "$REPO_URL" "$PROJECT_NAME"
