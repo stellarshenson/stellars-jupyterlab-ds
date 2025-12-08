b5c5cd81f65a9395b6aa6dc04dfbd941d8eafa7c
stellarshenson <konrad.jelen@gmail.com>
2025-12-08 23:07:07 +0100

    fix: run user startup scripts as bundle with single log file

--- services/jupyterlab/conf/bin/start-platform.d/58_start_lab_scripts.sh
+++ services/jupyterlab/conf/bin/start-platform.d/58_start_lab_scripts.sh
@@ -2,7 +2,8 @@
 # ----------------------------------------------------------------------------------------
 # Run Local Scripts from user home
 #
-# This script runs all available scripts in user home directory
+# This script runs all available scripts in user home directory as a bundle
+# Output is logged to ~/.local/start-platform.out
 # ----------------------------------------------------------------------------------------
 
 # check if enabled
@@ -10,16 +11,33 @@ if [[ ${ENABLE_LOCAL_SCRIPTS} != 1 ]]; then
     exit 0
 fi
 
-# run series of local start scripts
-# (services will need to run in background)
+# run series of local start scripts as a bundle
 LOCAL_SCRIPTS_DIR="${LOCAL_SCRIPTS_DIR:-${HOME}/.local/start-platform.d}"
+LOCAL_SCRIPTS_LOG="${HOME}/.local/start-platform.out"
+
 if [[ -d "${LOCAL_SCRIPTS_DIR}" ]]; then
-    echo "Executing user startup scripts from ${LOCAL_SCRIPTS_DIR}"
-    for file in ${LOCAL_SCRIPTS_DIR}/*.sh; do
-	if [ -f "${file}" ] && [ -x "${file}" ]; then
-	    nohup "${file}" 2>&1 >${LOCAL_SCRIPTS_DIR}/${file}.out &
-	fi
+    # collect executable scripts
+    scripts=()
+    for file in "${LOCAL_SCRIPTS_DIR}"/*.sh; do
+        if [[ -f "${file}" ]] && [[ -x "${file}" ]]; then
+            scripts+=("${file}")
+        fi
     done
+
+    # run all scripts as a bundle if any found
+    if [[ ${#scripts[@]} -gt 0 ]]; then
+        echo "Executing user startup scripts from ${LOCAL_SCRIPTS_DIR}"
+        echo "Log file: ${LOCAL_SCRIPTS_LOG}"
+        nohup bash -c '
+            for script in "$@"; do
+                echo "=== Running: ${script} ==="
+                echo "Started: $(date)"
+                "${script}"
+                echo "Finished: $(date)"
+                echo ""
+            done
+        ' _ "${scripts[@]}" > "${LOCAL_SCRIPTS_LOG}" 2>&1 &
+    fi
 fi
 
 # EOF
