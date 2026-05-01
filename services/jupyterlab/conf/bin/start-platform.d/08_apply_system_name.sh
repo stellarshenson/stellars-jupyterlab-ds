e86236aa8228427fbc36eaabe9aaf804ed99d266
stellarshenson <konrad.jelen@gmail.com>
2026-05-01 22:03:46 +0200

    feat: drop upstream GitHub link from welcome-message when SYSTEM_NAME set
    The URL would point to a non-existent repo once rebranded, so delete
    the 'For more information visit:' line entirely instead of rewriting it.

--- services/jupyterlab/conf/bin/start-platform.d/08_apply_system_name.sh
+++ services/jupyterlab/conf/bin/start-platform.d/08_apply_system_name.sh
@@ -10,6 +10,11 @@ if [[ -z "${JUPYTERLAB_SYSTEM_NAME}" ]]; then
 fi
 
 echo "Rebranding welcome files to ${JUPYTERLAB_SYSTEM_NAME}"
+
+# Drop the upstream GitHub link from welcome-message.txt - the URL would point
+# to a non-existent repo once the system name is rebranded
+sudo sed -i '/^For more information visit:/d' /welcome-message.txt
+
 sudo sed -i "s/stellars-jupyterlab-ds/${JUPYTERLAB_SYSTEM_NAME}/g" \
     /welcome.html \
     /welcome-message.txt \
